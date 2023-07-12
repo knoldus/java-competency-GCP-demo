3a1b826e8e4dece2a44181e91ced8547f59b246a
ankit-mogha <ankit.mogha@nashtechglobal.com>
2023-07-12 11:48:12 +0530

    Added checkstyle

--- src/main/java/com/nashtech/JavaCompetencyDemoApplication.java
+++ src/main/java/com/nashtech/JavaCompetencyDemoApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaCompetencyDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaCompetencyDemoApplication.class, args);
-	}
+/**
+ * This method is the entry point for the JavaCompetencyDemoApplication.
+ * It calls the springApplication.run method to start the application.
+ *
+ * @param args an array of command-line arguments passed to
+ *                the application
+ */
+public static void main(final String[] args) {
+SpringApplication.run(JavaCompetencyDemoApplication.class, args);
+}
 
 }
