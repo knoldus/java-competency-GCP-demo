1fe9a45b36f11a556aa288c1829b083e11f16bc2
jasleenwahi <jasleen.wahi@knoldus.com>
2023-07-13 11:29:16 +0530

    changes made

--- scripts/azurepowershellscript.ps1
+++ scripts/azurepowershellscript.ps1
@@ -1,20 +1,29 @@
-$resourceGroupName="myResourceGroup"
+$resourceGroupName="myResourceGrp"
+$randomIdentifier = Get-Random
 $location = "East US"
 $ehubName="myEventHub"
-$namespaceName="myNamespace2023"
-$functionAppName = "AzureFunctionTriggerApp2023"
-$runtime = "powershell"
-$storageAccountName = "testdatastorage2023"
-$accountName = "mydb2023"
-$databaseName = "sql-db-2023"
+$namespaceName="myNamespace2023125"
+$functionAppName = "AzureFunction202314"
+$storageAccountName = "storageaccount232009"
+$accountName = "reactive-database-twenty-$randomIdentifier"
+$databaseName = "sql-db-twenty-six"
+$functionPlan = "functionAppName-plans"
+$skuPlan = "B1"
+$apiKind = "MongoDB"
 New-AzResourceGroup –Name $resourceGroupName –Location $location
 New-AzEventHubNamespace -ResourceGroupName $resourceGroupName -Name $namespaceName -Location $location
-New-AzEventHub -ResourceGroupName $resourceGroupName -NamespaceName $namespaceName -EventHubName $ehubName
+New-AzEventHub -ResourceGroupName $resourceGroupName -NamespaceName $namespaceName  -PartitionCount 4 -EventHubName $ehubName
 New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
-New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name "$functionAppName-plan" -Location $location -Tier Free
-New-AzFunctionApp -Name $functionAppName -ResourceGroupName $resourceGroupName -StorageAccount $storageAccountName -Runtime PowerShell -FunctionsVersion 4 -Location $location
-New-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName -Location $location -DefaultConsistencyLevel "Session"
-$database = New-AzCosmosDBSqlDatabase -AccountName $accountName -ResourceGroupName $resourceGroupName -Name $databaseName
+New-AzFunctionAppPlan -Name $functionPlan -ResourceGroupName $resourceGroupName -Location $location -Sku $skuPlan -WorkerType Linux
+New-AzFunctionApp -Name $functionAppName -StorageAccountName $storageAccountName -PlanName $functionPlan -ResourceGroupName $resourceGroupName -Runtime Java -RuntimeVersion 17 -OSType Linux -FunctionsVersion 4
+New-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName -Location $location -ApiKind $apiKind  -DefaultConsistencyLevel "Session"
+$database = New-AzCosmosDBMongoDBDatabase -AccountName $accountName -ResourceGroupName $resourceGroupName -Name $databaseName
 $account = Get-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName
 $connectionString = $account.DocumentEndpoint + ";AccountKey=" + (Get-AzCosmosDBAccountKey -ResourceGroupName $resourceGroupName -Name $accountName).PrimaryMasterKey
 Write-Output "Cosmos DB Connection String: $connectionString"
+$endpoint = (Get-AzCosmosDBAccount -Name $accountName -ResourceGroupName $resourceGroupName).DocumentEndpoint
+Write-Host $endpoint
+$key = (Get-AzCosmosDBAccountKey -Name $accountName -ResourceGroupName $resourceGroupName).PrimaryMasterKey
+Write-Host $key
+Update-AzFunctionAppSetting -Name $functionAppName -ResourceGroupName $resourceGroupName -AppSetting @{CosmosDB_Endpoint = $endpoint; CosmosDB_Key = $key}
+New-AzApplicationInsights -ResourceGroupName $resourceGroupName -Name $functionAppName -location $location
