917187f716d1bf8e246ca309990796a90c241aac
jasleenwahi <jasleen.wahi@knoldus.com>
2023-07-19 18:06:52 +0530

    added azure function to cloud folder and made the requested changes

--- cloud/azurefunction/src/main/java/com/knoldus/function/model/VehicleDetail.java
+++ cloud/azurefunction/src/main/java/com/knoldus/function/model/VehicleDetail.java
@@ -1,7 +1,15 @@
 package com.knoldus.function.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class VehicleDetail {
     Integer cardId;
     String brand;
@@ -11,85 +19,4 @@ public class VehicleDetail {
     Double mileage;
     Double price;
 
-    public Integer getCardId() {
-        return cardId;
-    }
-
-    public void setCardId(final Integer cardId) {
-        this.cardId = cardId;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(final String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(final String model) {
-        this.model = model;
-    }
-
-    public Long getYear() {
-        return year;
-    }
-
-    public void setYear(final Long year) {
-        this.year = year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(final String color) {
-        this.color = color;
-    }
-
-    public Double getMileage() {
-        return mileage;
-    }
-
-    public void setMileage(final Double mileage) {
-        this.mileage = mileage;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(final Double price) {
-        this.price = price;
-    }
-
-    public VehicleDetail() {
-    }
-
-    public VehicleDetail(final Integer cardId, final String brand, final String model, final Long year, final String color, final Double mileage, final Double price) {
-        this.cardId = cardId;
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.mileage = mileage;
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "CarDetails{" +
-                "cardId=" + cardId +
-                ", brand='" + brand + '\'' +
-                ", model='" + model + '\'' +
-                ", year=" + year +
-                ", color='" + color + '\'' +
-                ", mileage=" + mileage +
-                ", price=" + price +
-                '}';
-    }
 }
