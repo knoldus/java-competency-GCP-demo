eba53289a4d0bcaca6c687692e76a19c143a3d95
jasleenwahi <124977275+jasleenwahi@users.noreply.github.com>
2023-07-26 17:27:52 +0530

    Update Car.java

--- cloud/azurefunction/src/main/java/com/knoldus/function/model/Car.java
+++ cloud/azurefunction/src/main/java/com/knoldus/function/model/Car.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class VehicleDetail {
+public class Car {
     Integer carId;
     String brand;
     String model;
