1991cec138d9601ae9ea61fc78ac1b1916c2010c
SahilBabbar12 <sahil.babbar@knoldus.com>
2023-07-26 10:50:59 +0530

    modified the Car class and updated the packages

--- src/main/java/com/nashtech/model/Car.java
+++ src/main/java/com/nashtech/model/Car.java
@@ -1,12 +1,14 @@
 package com.nashtech.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Car {
     /**
      * The id of the car.
@@ -43,90 +45,4 @@ public class Car {
      */
     private Double price;
 
-
-    /**
-     * Sets the ID of the car.
-     *
-     * @param id The ID of the car.
-     * @return This Car instance.
-     */
-    public Car setCarId(final Long id) {
-        this.carId = id;
-        return this;
-    }
-
-    /**
-     * Sets the model of the car.
-     *
-     * @param model The model of the car.
-     * @return This Car instance.
-     */
-    public Car setCarModel(final String model) {
-        this.carModel = model;
-        return this;
-    }
-
-    /**
-     * Sets the brand of the car.
-     *
-     * @param carBrand The brand of the car.
-     * @return This Car instance.
-     */
-    public Car setBrand(final String carBrand) {
-        this.brand = carBrand;
-        return this;
-    }
-
-    /**
-     * Sets the manufacturing year of the car.
-     *
-     * @param carYear The manufacturing year of the car.
-     * @return This Car instance.
-     */
-    public Car setYear(final Integer carYear) {
-        this.year = carYear;
-        return this;
-    }
-
-    /**
-     * Sets the color of the car.
-     *
-     * @param carColor The color of the car.
-     * @return This Car instance.
-     */
-    public Car setColor(final String carColor) {
-        this.color = carColor;
-        return this;
-    }
-
-    /**
-     * Sets the mileage of the car.
-     *
-     * @param carMileage The mileage of the car.
-     * @return This Car instance.
-     */
-    public Car setMileage(final Double carMileage) {
-        this.mileage = carMileage;
-        return this;
-    }
-
-    /**
-     * Sets the price of the car.
-     *
-     * @param carPrice The price of the car.
-     * @return This Car instance.
-     */
-    public Car setPrice(final Double carPrice) {
-        this.price = carPrice;
-        return this;
-    }
-
-    /**
-     * Builds and returns the Car instance with the provided attributes.
-     *
-     * @return This Car instance.
-     */
-    public Car build() {
-        return this;
-    }
 }
