44f7d0215baa9e08d820691117f164268873688c
jasleenwahi <jasleen.wahi@knoldus.com>
2023-08-03 16:42:26 +0530

    added test cases

--- src/test/java/com/nashtech/service/ReactiveDataServiceImplTest.java
+++ src/test/java/com/nashtech/service/ReactiveDataServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.nashtech.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.*;
 import com.nashtech.model.Car;
 import com.nashtech.model.CarBrand;
@@ -14,6 +15,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -69,5 +75,33 @@ public class ReactiveDataServiceImplTest {
                 .expectNext(brand1)
                 .expectNext(brand2)
                 .verifyComplete();
+    }
+
+    @Test
+    void testGetAllBrands_NoDuplicateBrandsReturned() {
+        // Prepare mock data
+        CarBrand brand1 = new CarBrand("BMW");
+        CarBrand brand2 = new CarBrand("Toyota");
+        CarBrand brand3 = new CarBrand("Mercedes");
+
+        // Mock the reactiveDataService to return Flux with duplicate brands
+        when(reactiveDataService.getAllBrands()).thenReturn(Flux.just(brand1, brand2, brand1, brand3, brand2));
+
+        // Run the test
+        final Flux<CarBrand> result = reactiveDataService.getAllBrands();
+
+        // Convert Flux to List
+        List<CarBrand> brandList = result.collectList().block();
+
+        // Verify the results
+        StepVerifier.create(result)
+                .recordWith(ArrayList::new) // Record all elements in an ArrayList
+                .expectNextCount(5) // Expecting 5 items in the Flux
+                .consumeRecordedWith(brands -> {
+                    // Convert the list to a Set to check for duplicates
+                    Set<CarBrand> uniqueBrands = new HashSet<>(brands);
+                    assertThat(uniqueBrands.size()).isEqualTo(3).isSameAs(brandList);
+                });
+
     }
 }
