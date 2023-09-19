2b11fee48ca3a25913d787753ca738f6a20c88de
Ankit Mogha <76722619+ankit-mogha@users.noreply.github.com>
2023-09-19 16:09:38 +0530

    Updated application-firestore.yml (#57)
    * Updated application-firestore.yml
    
    * updated gcp_pipeline.yml, gcp_Infra script

--- cloud/scripts/GCP-Infra.sh
+++ cloud/scripts/GCP-Infra.sh
@@ -3,18 +3,27 @@
 # Set your GCP project ID and other variables
 PROJECT_ID="sustained-digit-399111"
 CLUSTER_NAME="cluster-gcp-deployment"
-REGION="us-central1-c"
+REGION="us-central1"
 TOPIC_NAME="Vehicle"
 DB_REGION="us-east1"
 DB_TYPE="firestore-native"
-
+ARTIFACTORY_NAME="gcp-demo-app"
+ARTIFACTORY_FORMAT="docker"
 # Enable required APIs
 gcloud services enable container.googleapis.com pubsub.googleapis.com cloudfunctions.googleapis.com firestore.googleapis.com cloudbuild.googleapis.com eventarc.googleapis.com run.googleapis.com artifactregistry.googleapis.com
+
+# Create Github Container Registry
+gcloud artifacts repositories create $ARTIFACTORY_NAME --repository-format=$ARTIFACTORY_FORMAT --location=$REGION
+
 # Create a GKE cluster
 gcloud container clusters create $CLUSTER_NAME --num-nodes=2 --region=$REGION --scopes "https://www.googleapis.com/auth/cloud-platform"
+
+# Get Cluster Credentials
 gcloud container clusters get-credentials $CLUSTER_NAME --zone $REGION --project $PROJECT_ID
+
 # Create a Pub/Sub topic
 gcloud pubsub topics create $TOPIC_NAME --project=$PROJECT_ID
+
 #Create Firestore DB
 gcloud alpha firestore databases create --location=$DB_REGION --type=$DB_TYPE
 
